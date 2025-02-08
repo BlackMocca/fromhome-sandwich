d84bf3c8704fd7dde4aa708bc9e0a22226c022b4
Teeradet Phondetparinya <teeradet.phon@innovasive.co.th>
2025-02-08 15:26:46 +0700

    add customer modal

--- web/features/presentation/transaction/components/receipt.tsx
+++ web/features/presentation/transaction/components/receipt.tsx
@@ -1,6 +1,11 @@
-import _ from "lodash"
+import _ from "lodash";
 import React, { useEffect, useState, useMemo, useRef } from "react";
-import { Recepit, RecepitPreview, RecepitProduct, newRecepit } from '@/features/domain/receipt.type'
+import {
+  Recepit,
+  RecepitPreview,
+  RecepitProduct,
+  newRecepit,
+} from "@/features/domain/receipt.type";
 import Image from "next/image";
 import ButtonLayout from "@/features/core/layouts/button.layout";
 import { ProductOption } from "@/features/domain/product.type";
@@ -9,170 +14,284 @@ import { formatDate, ParseYear, Format } from "@/features/core/helper";
 import html2canvas from "html2canvas";
 
 interface IReceiptPreview {
-  receipt: Recepit | RecepitPreview
-  onClearProduct?(): void
-  onUpdateReceiptPreview?(newData: Partial<RecepitPreview>): void
+  receipt: Recepit | RecepitPreview;
+  onClearProduct?(): void;
+  onUpdateReceiptPreview?(newData: Partial<RecepitPreview>): void;
 }
 
+interface ICustomerInfo {
+  customer_name: string;
+}
+
+interface modalInputCustomerInfoProps {
+  isDisplay: boolean;
+  onCancel: () => void;
+  callbackOnSubmit: (info: Partial<ICustomerInfo>) => void;
+}
+
+const ModalInputCustomerInfo = ({
+  isDisplay,
+  onCancel,
+  callbackOnSubmit,
+}: modalInputCustomerInfoProps): React.JSX.Element => {
+  const [customerName, setCustomerName] = useState("");
+
+  const handleSubmit = () => {
+    callbackOnSubmit({ customer_name: customerName });
+  };
+
+  return (
+    <div
+      className={classNames(
+        "flex justify-center items-center fixed top-0 left-0 w-screen h-screen bg-black bg-opacity-50 backdrop-blur-md",
+        { hidden: !isDisplay }
+      )}
+      onClick={(e) => {
+        if (e.target === e.currentTarget) onCancel();
+      }}
+    >
+      <div className="flex flex-col w-1/2 bg-white px-4 py-8 gap-4">
+        <fieldset className="border-2 border-primary rounded-[27px] p-4 text-base">
+          <legend className="text-lg font-semibold px-2 py-1 rounded-md">
+            Customer Information
+          </legend>
+          <label className="py-2">
+            ชื่อลูกค้า
+            <input
+              type="text"
+              className="w-full border border-primary rounded-lg px-3 py-2"
+              value={customerName}
+              onChange={(e) => setCustomerName(e.target.value)}
+            />
+          </label>
+        </fieldset>
+        <div className="flex flex-1 flex-row gap-4 justify-evenly">
+          <ButtonLayout
+            title="ยกเลิก"
+            size="md"
+            onclick={() => {
+              onCancel();
+            }}
+            isActive={false}
+            buttonStyleType="error"
+            style="min-w-[33%]"
+          />
+          <ButtonLayout
+            title="ตกลง"
+            size="md"
+            onclick={handleSubmit}
+            isActive={false}
+            buttonStyleType="success"
+            style="min-w-[33%]"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
 export default function ReceiptPreview(props: IReceiptPreview) {
-    const [receipt, setReceipt] = useState<Recepit | undefined >(undefined)
-    const captureRef = useRef<HTMLDivElement>(null);
-    const { grandTotal, date, receip_no } = useMemo(() => {
-      if (receipt) {
-        return {
-          grandTotal: receipt.grand_total,
-          date: formatDate(receipt.created_at, ParseYear.THAI, Format.DISPLAY_TIMESTAMP),
-          receip_no: receipt.receipt_no
-        }
-      }
-      switch (props.receipt.kind) {
-        case "printing":
-          return {
-            grandTotal: (props.receipt as Recepit).grand_total,
-            date: formatDate((props.receipt as Recepit).created_at, ParseYear.THAI, Format.DISPLAY_TIMESTAMP),
-            receip_no: (props.receipt as Recepit).receipt_no,
-          };
-        case "preview":
-          return {
-            grandTotal: (props.receipt as RecepitPreview).calculateGrandTotal(),
-            date: "",
-            receip_no: "",
-          };
-        default:
-          return { grandTotal: 0, date: "", receip_no: "" };
-      }
-    }, [props.receipt, receipt]);
-
-    // -----------------------------------------------
-    // Handle
-    // -----------------------------------------------
-    const onSubmit = () => {
-      let recepit = newRecepit((props.receipt as RecepitPreview))
-      setReceipt(recepit)
+  const [receipt, setReceipt] = useState<Recepit | undefined>(undefined);
+  const [showModal, setShowModal] = useState(false);
+  const captureRef = useRef<HTMLDivElement>(null);
+  const { grandTotal, date, receip_no } = useMemo(() => {
+    if (receipt) {
+      return {
+        grandTotal: receipt.grand_total,
+        date: formatDate(
+          receipt.created_at,
+          ParseYear.THAI,
+          Format.DISPLAY_TIMESTAMP
+        ),
+        receip_no: receipt.receipt_no,
+      };
     }
+    switch (props.receipt.kind) {
+      case "printing":
+        return {
+          grandTotal: (props.receipt as Recepit).grand_total,
+          date: formatDate(
+            (props.receipt as Recepit).created_at,
+            ParseYear.THAI,
+            Format.DISPLAY_TIMESTAMP
+          ),
+          receip_no: (props.receipt as Recepit).receipt_no,
+        };
+      case "preview":
+        return {
+          grandTotal: (props.receipt as RecepitPreview).calculateGrandTotal(),
+          date: "",
+          receip_no: "",
+        };
+      default:
+        return { grandTotal: 0, date: "", receip_no: "" };
+    }
+  }, [props.receipt, receipt]);
 
-    const download = async () => {
-      if (captureRef.current) {
-        const style = document.createElement('style');
-        document.head.appendChild(style);
-        style.sheet?.insertRule('body > div:last-child img { display: inline-block; }');
-        captureRef.current.scrollTop = captureRef.current.scrollHeight
-        const canvas = await html2canvas(captureRef.current, {
-          scale: window.devicePixelRatio,
-          useCORS: true,
-          x:0,
-          y:0,
-          logging: true, 
-                    
-          imageTimeout: 0,
-        });
-        console.log(canvas)
-        
-        const image = canvas.toDataURL("image/jpeg", 100);
-        const link = document.createElement("a");
-        link.href = image;
-        link.download =  `${receipt?.receipt_no !== "" ? receipt?.receipt_no: "screenshot"}.jpg`;
-        link.click();
-      }
-    };
+  // -----------------------------------------------
+  // Handle
+  // -----------------------------------------------
+  const onSubmit = () => {
+    let recepit = newRecepit(props.receipt as RecepitPreview);
+    setReceipt(recepit);
+  };
 
-    // -----------------------------------------------
-    // RENDER
-    // -----------------------------------------------
-    return (
-      <div className={classNames(
-          "flex flex-col flex-between w-[304px] border border-black rounded-[27px] overflow-y-auto text-black text-xs scrollbar-hide",
-          {"flip-y": receipt !== undefined},
+  const onCustomerInfoSubmit = (customerInfo: Partial<ICustomerInfo>) => {
+    props.onUpdateReceiptPreview?.(customerInfo);
+    setShowModal(false);
+  };
+
+  const download = async () => {
+    if (captureRef.current) {
+      const style = document.createElement("style");
+      document.head.appendChild(style);
+      style.sheet?.insertRule(
+        "body > div:last-child img { display: inline-block; }"
+      );
+      captureRef.current.scrollTop = captureRef.current.scrollHeight;
+      const canvas = await html2canvas(captureRef.current, {
+        scale: window.devicePixelRatio,
+        useCORS: true,
+        x: 0,
+        y: 0,
+        logging: true,
+
+        imageTimeout: 0,
+      });
+      console.log(canvas);
+
+      const image = canvas.toDataURL("image/jpeg", 100);
+      const link = document.createElement("a");
+      link.href = image;
+      link.download = `${
+        receipt?.receipt_no !== "" ? receipt?.receipt_no : "screenshot"
+      }.jpg`;
+      link.click();
+    }
+  };
+
+  // -----------------------------------------------
+  // RENDER
+  // -----------------------------------------------
+  return (
+    <div
+      className={classNames(
+        "flex flex-col flex-between w-[304px] border border-black rounded-[27px] overflow-y-auto text-black text-xs scrollbar-hide",
+        { "flip-y": receipt !== undefined }
+      )}
+    >
+      <div ref={captureRef} className="px-[14px] py-[24px]">
+        {/* head bill */}
+        <div className="flex py-[16px] justify-center relative">
+          <img src={props.receipt.merchant_logo} width={100} height={89} />
+        </div>
+        {props.receipt.customer_name ? (
+          <div className="flex w-full py-[8px] justify-center">
+            <p className="font-semiBold text-4xl">
+              {props.receipt.customer_name}
+            </p>
+          </div>
+        ) : (
+          <></>
         )}
-      >
-        <div ref={captureRef} className="px-[14px] py-[24px]">
+        <div className="flex flex-1 flex-col gap-1 py-[16px]">
+          <p>Date: {date}</p>
+          <p>Receipt No: {receip_no}</p>
+        </div>
 
-          {/* head bill */}
-          <div className="flex py-[16px] justify-center relative">
-            <img src={props.receipt.merchant_logo} width={100} height={89} />
-          </div>
-          { props.receipt.customer_name ? (
-            <div className="flex w-full py-[8px] justify-center">
-              <p className="font-semiBold text-4xl">{props.receipt.customer_name}</p>
-            </div>
-          ): <></>}
-          <div className="flex flex-1 flex-col gap-1 py-[16px]">
-            <p>Date: {date}</p>
-            <p>Receipt No: {receip_no}</p>
-          </div>
+        <div className="w-full border-b border-black border-dashed"></div>
 
-          <div className="w-full border-b border-black border-dashed"></div>
-
-          {/* body bill */}
-          <div className="flex flex-col py-[16px]">
-            {_.map(props.receipt.products, (product: RecepitProduct, i: number) => (
+        {/* body bill */}
+        <div className="flex flex-col py-[16px]">
+          {_.map(
+            props.receipt.products,
+            (product: RecepitProduct, i: number) => (
               <div className="flex flex-row justify-between py-1" key={i}>
                 <div className="text-left">
                   <p className="text-sm">{product.name}</p>
-                  <p>{product.amount} x ฿{product.price.toFixed(2)}</p>
+                  <p>
+                    {product.amount} x ฿{product.price.toFixed(2)}
+                  </p>
 
-                  {_.map(product.options, (option: ProductOption, j: number) => (
-                    <p className="pl-4" key={j}>- {option.name}</p>
-                  ))}
-
+                  {_.map(
+                    product.options,
+                    (option: ProductOption, j: number) => (
+                      <p className="pl-4" key={j}>
+                        - {option.name}
+                      </p>
+                    )
+                  )}
                 </div>
                 <div className="text-right">
                   <p className="text-sm">{product.calculatePrice()}</p>
                 </div>
               </div>
-            ))}
-          </div>
-            
-          <div className="w-full border-b border-black border-dashed"></div>
-
-          {/* result bill */}
-          <div className="flex justify-between py-[16px] text-base leading-8">
-            <div className="text-left">
-              <p>ราคารวม</p>
-            </div>
-            <div className="text-right">
-              <p>{grandTotal}</p>
-            </div>
-          </div>
+            )
+          )}
         </div>
 
-        {/* tail bill sohw only preview */}
-        <div className="flex flex-col gap-2 h-full text-2xl justify-end items-end px-[14px] pb-[24px]">
+        <div className="w-full border-b border-black border-dashed"></div>
+
+        {/* result bill */}
+        <div className="flex justify-between py-[16px] text-base leading-8">
+          <div className="text-left">
+            <p>ราคารวม</p>
+          </div>
+          <div className="text-right">
+            <p>{grandTotal}</p>
+          </div>
+        </div>
+      </div>
+
+      {/* tail bill sohw only preview */}
+      <div className="flex flex-col gap-2 h-full text-2xl justify-end items-end px-[14px] pb-[24px]">
         {receipt === undefined ? (
           <>
-            <ButtonLayout 
+            <ModalInputCustomerInfo
+              isDisplay={showModal}
+              onCancel={() => {
+                setShowModal(false);
+              }}
+              callbackOnSubmit={onCustomerInfoSubmit}
+            />
+            <ButtonLayout
               title="ล้างสินค้า"
               buttonStyleType="error"
               size="lg"
               isActive={false}
               onclick={() => props.onClearProduct?.()}
             />
-            <ButtonLayout 
+            <ButtonLayout
               title="เพิ่ม/แก้ไข ชื่อลูกค้า"
-              buttonStyleType={_.size(props.receipt.products) === 0 ? "disable":"primary"}
+              buttonStyleType={
+                _.size(props.receipt.products) === 0 ? "disable" : "primary"
+              }
               size="lg"
               isActive={false}
-              onclick={() => props.onUpdateReceiptPreview?.({customer_name: "ASDC"})}
+              onclick={() => setShowModal(true)}
             />
-            <ButtonLayout 
+            <ButtonLayout
               title="สร้างบิล"
-              buttonStyleType={_.size(props.receipt.products) === 0 ? "disable":"success"}
+              buttonStyleType={
+                _.size(props.receipt.products) === 0 ? "disable" : "success"
+              }
               size="lg"
               isActive={false}
               onclick={() => onSubmit()}
             />
           </>
-        ):
-          <ButtonLayout 
+        ) : (
+          <ButtonLayout
             title="Download"
-            buttonStyleType={_.size(props.receipt.products) === 0 ? "disable":"primary"}
+            buttonStyleType={
+              _.size(props.receipt.products) === 0 ? "disable" : "primary"
+            }
             size="lg"
             isActive={false}
-            onclick={ async () => await download()}
+            onclick={async () => await download()}
           />
-        }
-        </div>
+        )}
       </div>
-    );
-  }
-  
+    </div>
+  );
+}
