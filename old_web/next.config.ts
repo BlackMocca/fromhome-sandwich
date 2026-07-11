60ddd97b64872d80b31a5232409f84fc1a3f2cad
blackmocca <teeradet.huag@gmail.com>
2026-07-11 20:06:18 +0700

    patch .env.example

--- old_web/next.config.ts
+++ old_web/next.config.ts
@@ -7,9 +7,6 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: false, // will prevent building if there are TypeScript errors
   },
-  publicRuntimeConfig: {
-    NEXT_PUBLIC_APP_DOMAIN: process.env.APP_DOMAIN
-  },
 };
 
 export default nextConfig;
